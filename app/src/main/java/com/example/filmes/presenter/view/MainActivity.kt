7e85feb6bde6776609d0570f145941664e4d8360
Maria-Alane <maria.alane.bs@gmail.com>
2022-06-13 11:28:48 -0300

    Tratamento de erro, progressbar e alert dialog error

--- app/src/main/java/com/example/filmes/presenter/view/MainActivity.kt
+++ app/src/main/java/com/example/filmes/presenter/view/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.filmes.presenter.view
 
+import android.R
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.filmes.data.api.RetrofitFilme
 import com.example.filmes.data.model.FilmeResponse
@@ -11,6 +16,7 @@ import com.example.filmes.databinding.ActivityMainBinding
 import com.example.filmes.presenter.adapter.FilmeItemAdapter
 import com.example.filmes.presenter.viewmodel.MainViewModel
 import com.example.filmes.utils.Status
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -50,16 +56,43 @@ class MainActivity : AppCompatActivity() {
             it?.let { resource ->
                 when (resource.status) {
                     Status.SUCCESS -> {
+                        filmes_list_recyclerview.visibility = View.VISIBLE
+                        progressBarMain.visibility = View.GONE
                         resource.data?.let { filmeResult -> filmeListAdapter.submitList(filmeResult.results) }
                     }
                     Status.ERROR -> {
-                        Log.d("INFO:", "OCORREU UM ERRO")
+                        filmes_list_recyclerview.visibility = View.VISIBLE
+                        progressBarMain.visibility = View.GONE
+                        alertError()
                     }
                     Status.LOADING -> {
+                        progressBarMain.visibility = View.VISIBLE
+                        filmes_list_recyclerview.visibility = View.GONE
                         Log.d("INFO:", "LOADING")
                     }
                 }
             }
         }
+    }
+
+    fun alertError() {
+        AlertDialog.Builder(this)
+            .setIcon(R.drawable.ic_dialog_alert)
+            .setTitle("Ocorreu um erro: ")
+            .setMessage("Deseja tentar novamente?")
+            .setPositiveButton(
+                "Tentar novamente",
+                DialogInterface.OnClickListener { dialogInterface, i ->
+                    setupViewModel()
+                }
+            )
+            .setNegativeButton(
+                "Sair",
+                DialogInterface.OnClickListener { dialogInterface, i ->
+                    dialogInterface.dismiss()
+                    Toast.makeText(this, "Tente novamente mais tarde.", Toast.LENGTH_LONG).show()
+                }
+            )
+            .show()
     }
 }
